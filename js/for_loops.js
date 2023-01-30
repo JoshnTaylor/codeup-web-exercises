fda3f6c7ede70763375baae7349a36345505aacd
Joshua Taylor <joshua.n.taylor65@gmail.com>
2023-01-30 13:36:56 -0700

    Finished the first portion of loop exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -13,3 +13,22 @@ function showMultiplicationTable(x) {
         console.log (x + " x 9 = " + (x * 9)) +
         console.log (x + " x 10 = " + (x * 10)))
 }
+for (var y = 0; y < 10; y++ ) {
+    var random = Math.floor(Math.random() * (200 - 20 + 1) + 20)
+    if (random % 2 !== 0) {
+        console.log(random + " is odd!")
+    } else {
+        console.log(random + " is even!")
+    }
+}
+for (var a = 1; a < 10; a++) {
+    var string = "";
+    for (var b = 1; b <= a; b++) {
+        string += a;
+}
+    console.log(string)
+}
+for (var c = 100; c > 0; c -= 5) {
+    console.log(c)
+}
+
