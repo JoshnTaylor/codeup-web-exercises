6ffc2e6bfb1e75d04a31927427a7d2aa7b742741
Joshua Taylor <joshua.n.taylor65@gmail.com>
2023-01-30 11:45:56 -0700

    First exercise done within for-loopss

---
+++ js/for_loops.js
@@ -0,0 +1,15 @@
+"use strict";
+// console.log("TEST")
+function showMultiplicationTable(x) {
+    return (console.log("You chose to see the multiplication table for " + x) +
+        console.log (x + " x 1 = " + (x * 1)) +
+        console.log (x + " x 2 = " + (x * 2)) +
+        console.log (x + " x 3 = " + (x * 3)) +
+        console.log (x + " x 4 = " + (x * 4)) +
+        console.log (x + " x 5 = " + (x * 5)) +
+        console.log (x + " x 6 = " + (x * 6)) +
+        console.log (x + " x 7 = " + (x * 7)) +
+        console.log (x + " x 8 = " + (x * 8)) +
+        console.log (x + " x 9 = " + (x * 9)) +
+        console.log (x + " x 10 = " + (x * 10)))
+}
